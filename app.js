58817ca64bec582a588faaeca67cce3484cea806
Simo Haakana <simo.haakana@eficode.com>
2016-11-14 18:03:13 +0200

    Bit better example how todo app would work

--- app.js
+++ app.js
@@ -13,20 +13,35 @@ app.use(bodyParser());
 
 
 router.get('/', function *(next) {
-  this.body = {demo: 'Demo'};
+  const demo = {
+    todos: [
+      {
+        id: 1,
+        title: "example task",
+        done: false
+      },
+      {
+        id: 2,
+        title: "example task 2",
+        done: true
+      }
+    ]
+  };
+  this.body = demo;
 });
 
 router.post('/', function *(next) {
   // THIS IS JUST STUPID EXAMPLE :D
   const body = this.request.body;
   var response = {};
-  if (body.demo) {
-    console.log(body.demo);
-    response.demo = body.demo;
+  if (body.title) {
+    response.title = body.title;
+    response.id = 3,
+    response.done = false,
     response.msg = "success";
   }
   else {
-    response = {msg: "FAIL"}
+    response.msg = "FAIL";
   }
   this.body = response;
 });

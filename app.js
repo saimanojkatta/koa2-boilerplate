eb6a8c7cdff2d71436dc221a8b289cbc04a2b2cd
Simo Haakana <simo.haakana@eficode.com>
2016-11-14 17:55:25 +0200

    Adding first POST controller to app

--- app.js
+++ app.js
@@ -2,15 +2,33 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import logger from 'koa-logger';
 import json from 'koa-json';
+import bodyParser from 'koa-bodyparser';
 const app = new Koa();
 const router = new Router();
 
 // Logger
 app.use(logger());
 app.use(json());
+app.use(bodyParser());
+
 
 router.get('/', function *(next) {
   this.body = {demo: 'Demo'};
+});
+
+router.post('/', function *(next) {
+  // THIS IS JUST STUPID EXAMPLE :D
+  const body = this.request.body;
+  var response = {};
+  if (body.demo) {
+    console.log(body.demo);
+    response.demo = body.demo;
+    response.msg = "success";
+  }
+  else {
+    response = {msg: "FAIL"}
+  }
+  this.body = response;
 });
 
 app.use(router.routes())
